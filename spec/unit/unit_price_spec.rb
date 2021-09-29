0ad869fc357ad6360ec56cb98bb45e0180abac78
Jacob <ja7davis@gmail.com>
2021-09-29 17:48:26 -0500

    Added attributes to books and spec tests

--- spec/unit/unit_price_spec.rb
+++ spec/unit/unit_price_spec.rb
@@ -1,4 +1,4 @@
-# location: spec/unit/unit_spec.rb
+# location: spec/unit/unit_price_spec.rb
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
@@ -6,12 +6,12 @@ RSpec.describe Book, type: :model do
     described_class.new(title: 'Pride and Prejudice', author: 'Jane Austen', price: 12, published_date: '1813-01-28')
   end
 
-  it 'is valid with valid attributes' do
+  it 'is valid with valid price' do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
-    subject.title = nil
+  it 'is not valid without a price' do
+    subject.price = nil
     expect(subject).not_to be_valid
   end
 end
